d60ce42a7404aa635d9b6c3c6f9cd12bf6227dd0
Steven Clontz <steven.clontz@gmail.com>
2025-05-17 22:51:40 +0000

    add empty set property

--- Game/Levels/SpaceWorld.lean
+++ Game/Levels/SpaceWorld.lean
@@ -3,10 +3,12 @@ import Game.Levels.SpaceWorld.L02
 import Game.Levels.SpaceWorld.L03
 import Game.Levels.SpaceWorld.L04
 import Game.Levels.SpaceWorld.L05
+import Game.Levels.SpaceWorld.L06
 
 World "SpaceWorld"
 Title "Space World"
 
 Introduction "
-This introduction is shown before one enters level 1 of the demo world. Use markdown.
+Let's get started by exploring the three basic axioms for
+topological spaces.
 "
---
+++ Game/Levels/SpaceWorld/L06.lean
@@ -0,0 +1,39 @@
+import Game.Metadata
+
+World "SpaceWorld"
+Level 6
+Title "An Empty Feeling"
+
+Introduction "
+You might think we forgot a common \"axiom\" of topological
+spaces: the empty set must always be open. But in fact, this
+is guaranteed by the other three axioms.
+"
+
+/--
+Proof that `⋃₀ ∅ = ∅`.
+-/
+TheoremDoc Set.sUnion_empty as "sUnion_empty" in "Set"
+NewTheorem Set.sUnion_empty
+
+open Set Topology Nat
+
+variable {X : Type}
+
+Statement (h: TopologicalSpace X) : IsOpen (∅: Set X) := by
+  Hint "
+  You can use `sUnion_empty` to rewrite this goal into a goal
+  that can be addressed by one of the three axioms.
+  "
+  rw [← sUnion_empty]
+  Hint "
+  Now apply the right topological axiom.
+  "
+  apply h.isOpen_sUnion
+  Hint "
+  Hmm, this seems rather trivial! Maybe lean can knock this out
+  with one word?
+  "
+  simp
+
+Conclusion "Awesome job!"

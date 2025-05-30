e9658b9fc3e8a9a82a00b30dc6189c193cf8d682
Jim Fowler <fowler@math.osu.edu>
2025-05-30 11:15:23 -0400

    including proof for discrete spaces

--- Game/Levels/W03/L01.lean
+++ Game/Levels/W03/L01.lean
@@ -2,24 +2,13 @@ import Game.Metadata
 
 World "ContinuityWorld"
 Level 1
-Title "Mapping into the indiscrete"
+Title "Mapping out of the discrete"
 
 Introduction "
 Our first task is to prove that every function
 from a *discrete* topological space to a topological space
 is continuous.
 "
-
-/-- yada yada -/
-TheoremDoc absurd as "absurd" in "Logic"
-/-- yada yada -/
-TheoremDoc TopologicalSpace.mk as "TopologicalSpace.mk" in "Topology"
-/-- yada yada -/
-TheoremDoc Set.eq_univ_iff_forall as "eq_univ_iff_forall" in "Set"
-
-NewTheorem TopologicalSpace.mk Set.eq_univ_iff_forall absurd
-
-NewTactic right left by_cases obtain «have»
 
 open Set Topology
 variable {X : Type}
@@ -28,9 +17,12 @@ variable {f : X → Y}
 
 variable [TopologicalSpace X]
 variable [TopologicalSpace Y]
-variable [DiscreteTopology Y]
+
+variable (h : ∀ (s : Set X), IsOpen s)
 
 Statement : Continuous f := by
-  sorry
+  rw [continuous_def]
+  rintro s hs
+  apply h
 
-Conclusion "Whew, you did it!"
+Conclusion "You did it!"
